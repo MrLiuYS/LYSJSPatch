7f95ac476df665c45305925be84920f621d7ebe9
Mr.Liu <3050700400@qq.com>
2016-09-05 21:41:04 +0800

    创建button , 使用类方法跳转

--- LYSJSPatch/LYS_JSFile/ButtonViewCtrl.js
+++ LYSJSPatch/LYS_JSFile/ButtonViewCtrl.js
@@ -1,18 +1,84 @@
-
+require('UIButton,UIColor,NSNumber');
 
 defineClass('ButtonViewCtrl : UIViewController ', {
-            
-            
-            viewDidLoad: function() {
-            
-            self.ORIGviewDidLoad();
-            
-            self.setTitle("UIButton")
-            
-            self.view().setBackgroundColor(UIColor.whiteColor());
-            
-            },
-            
-            
-            
-            });
+
+   /**
+    * 实例方法
+    */
+
+   viewDidLoad: function() {
+
+      self.ORIGviewDidLoad();
+
+      self.setTitle("UIButton")
+
+      self.view().setBackgroundColor(UIColor.whiteColor());
+
+      var button = self.createButton();
+
+      self.view().addSubview(button);
+
+      button.mas__makeConstraints(block('MASConstraintMaker*', function(make) {
+
+         make.top().equalTo()(self.view()).offset()(10);
+         make.left().equalTo()(self.view()).offset()(10);
+         make.right().equalTo()(self.view()).offset()(-10);
+         make.height().equalTo()(NSNumber.numberWithInt(40));
+
+      }));
+
+
+   },
+
+   /**
+    *  创建按钮
+    */
+   createButton: function() {
+
+      var button = UIButton.alloc().init();
+
+      button.setBackgroundColor(UIColor.lightGrayColor());
+
+      button.setTitle_forState("按钮", 0);
+
+      /**
+       *  JS使用字符串代表 Selector
+       */
+      button.addTarget_action_forControlEvents(self, "clickButton:", 5);
+
+      return button;
+
+   },
+
+
+   clickButton: function(aButton) {
+
+
+      var alertView = require('UIAlertView')
+         .alloc()
+         .initWithTitle_message_delegate_cancelButtonTitle_otherButtonTitles(
+            "Alert",
+            "点击了按钮",
+            self,
+            "OK",
+            null
+         )
+      alertView.show()
+
+
+   },
+
+
+}, {
+
+   /**
+    * 类方法
+    */
+   pushInViewController: function(aViewController) {
+
+      var ctrl = ButtonViewCtrl.alloc().init();
+
+      aViewController.navigationController().pushViewController_animated(ctrl, YES);
+
+   },
+});
--- LYSJSPatch/LYS_JSFile/main.js
+++ LYSJSPatch/LYS_JSFile/main.js
@@ -1,4 +1,3 @@
-
 /**
  *  导入js文件
  */
@@ -12,108 +11,103 @@ require('UITableView,UIColor,UITableViewCell');
  *  defineClass() 第二个参数为类新增 property，格式为字符串数组，使用时与 OC property 接口一致
  */
 defineClass('ViewController : UIViewController <UITableViewDelegate,UITableViewDataSource>', ['mainTable'], {
-            
-            
-            //<<<<<<
-            
-            loadView: function() {
-            
-            self.super().loadView()
-            
-            /**
-             *  添加新的 Property
-             */
-            self.setMainTable( UITableView.alloc().init() )
-            
-            },
-            
-            
-            lys__viewDidLoad: function() {
-            
-            var pMainTable = self.mainTable();
-            
-            pMainTable.setDelegate(self);
-            pMainTable.setDataSource(self);
-            
-            
-            pMainTable.setBackgroundColor(UIColor.greenColor());
-            
-            self.view().addSubview(pMainTable);
-            
-            pMainTable.mas__makeConstraints(block('MASConstraintMaker*', function(make) {
-                                                  
-                                                  make.top().equalTo()(self.view()).offset()(10);
-                                                  make.left().equalTo()(self.view()).offset()(10);
-                                                  make.right().equalTo()(self.view()).offset()(-10);
-                                                  make.bottom().equalTo()(self.view()).offset()(-10);
-                                                  
-                                                  
-                                                  }));
-            
-            },
-            
-            
-            /**
-             *  <UITableViewDelegate,UITableViewDataSource>
-             */
-            numberOfSectionsInTableView: function(tableView) {
-            
-            return 1;
-            
-            },
-            tableView_numberOfRowsInSection: function(tableView, section) {
-            
-            return 10;
-            
-            },
-            tableView_cellForRowAtIndexPath: function(tableView, indexPath) {
-            
-            var cell = tableView.dequeueReusableCellWithIdentifier("cell")
-            
-            if (!cell) {
-            
+
+
+    //<<<<<<
+
+    loadView: function() {
+
+        self.super().loadView()
+
+        /**
+         *  添加新的 Property
+         */
+        self.setMainTable(UITableView.alloc().init())
+
+    },
+
+
+    lys__viewDidLoad: function() {
+
+        var pMainTable = self.mainTable();
+
+        pMainTable.setDelegate(self);
+        pMainTable.setDataSource(self);
+
+
+        pMainTable.setBackgroundColor(UIColor.greenColor());
+
+        self.view().addSubview(pMainTable);
+
+        pMainTable.mas__makeConstraints(block('MASConstraintMaker*', function(make) {
+
+            make.top().equalTo()(self.view()).offset()(10);
+            make.left().equalTo()(self.view()).offset()(10);
+            make.right().equalTo()(self.view()).offset()(-10);
+            make.bottom().equalTo()(self.view()).offset()(-10);
+
+
+        }));
+
+    },
+
+
+    /**
+     *  <UITableViewDelegate,UITableViewDataSource>
+     */
+    numberOfSectionsInTableView: function(tableView) {
+
+        return 1;
+
+    },
+    tableView_numberOfRowsInSection: function(tableView, section) {
+
+        return 10;
+
+    },
+    tableView_cellForRowAtIndexPath: function(tableView, indexPath) {
+
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell")
+
+        if (!cell) {
+
             cell = UITableViewCell.alloc().initWithStyle_reuseIdentifier(0, "cell")
-            
-            }
-            
-            var text = "";
-            
-            switch (indexPath.row()) {
-            
+
+        }
+
+        var text = "";
+
+        switch (indexPath.row()) {
+
             case 0:
-            text = "UIButton";
-            break;
-            
+                text = "UIButton";
+                break;
+
             case 1:
-            text = "UILabel";
-            break;
-            
+                text = "UILabel";
+                break;
+
             default:
-            break;
-            
-            }
-            
-            cell.textLabel().setText(text);
-            
-            return cell;
-            
-            },
-            
-            tableView_didSelectRowAtIndexPath: function(tableView, indexPath) {
-            
-            
-            var ctrl = ButtonViewCtrl.alloc().init();
-            
-            self.navigationController().pushViewController_animated(ctrl, YES);
-            
-            
-            },
-            
-            
-            
-            //>>>>>>
-            
-            
-            });
+                break;
+
+        }
+
+        cell.textLabel().setText(text);
+
+        return cell;
+
+    },
+
+    tableView_didSelectRowAtIndexPath: function(tableView, indexPath) {
+
+        ButtonViewCtrl.pushInViewController(self);
 
 
+    },
+
+
+
+    //>>>>>>
+
+
+});
